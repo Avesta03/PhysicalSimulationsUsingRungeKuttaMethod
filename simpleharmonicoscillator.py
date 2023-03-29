74246bec26377cc516b9aa6fa70739d643a71929
Avesta03 <avestaafsharimehr@gmail.com>
2023-03-30 00:39:25 +0100

    Planning SHM oscillator

--- simpleharmonicoscillator.py
+++ simpleharmonicoscillator.py
@@ -1,3 +1,16 @@
+# Simulation of a Simple Harmonic Oscillator using Physics and the Runge-Kutta Method of Numerical Integration
+
 import numpy as np
 import matplotlib.pyplot as plt
 
+# F = kx 
+
+# parameters
+
+# time step and time span
+
+# initial conditions (of the system)
+
+# 
+
+# Runge-Kutta method to solve system of Ordinary Differential Equations
