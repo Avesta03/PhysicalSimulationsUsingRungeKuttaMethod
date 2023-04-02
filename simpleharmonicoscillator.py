04b6650275c187c7508282d7ec8a1773328acd2e
Avesta03 <avestaafsharimehr@gmail.com>
2023-04-02 20:56:50 +0100

    Implemented RK and noticed poss error

--- simpleharmonicoscillator.py
+++ simpleharmonicoscillator.py
@@ -18,6 +18,7 @@ t_end = 20.00
 
 # initial conditions (of the system)
 y0 = np.array([x0, v0])
+y = np.array([y0, v0]) # ?
 
 # defining a function that establishes/describes the system dynamics
 def f(t, y):
@@ -28,3 +29,15 @@ def f(t, y):
 # Runge-Kutta method to solve system of Ordinary Differential Equations
 def rk4th_step(f, t, y, dt):
     k1 = f(t, y)
+    k2 = f(t + dt/2, y + dt/2*k1)
+    k3 = f(t + dt/2, y + dt/2*k2)
+    k4 = f(t + dt, y + dt/k3)
+    return y + dt/6*(k1 + 2*k2 + 2*k3 + k4)
+
+t = np.arange(t_start, t_end, dt)
+t = np.zeros(len(t), len(y0))
+y[0] = y0
+
+for i in range(len(t)-1):
+    y[i+1] = rk4th_step(f, t[i], y[i], dt)
+
